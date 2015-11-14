fafd84e1148c0c9e6b5574ede87d1e0e4099c058
Mikhaelmurmur <stolovich@yahoo.com>
2015-11-14 22:47:47 +0200

    Bouncing done

---
+++ GameJamKPI/Assets/BouncingFloor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BouncingFloor : MonoBehaviour
+{
+
+    public float bounceFactor = 4f;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        other.gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        other.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 1, ForceMode2D.Impulse);
+    }
+}
--- GameJamKPI/Assets/PlayerController.cs
+++ GameJamKPI/Assets/PlayerController.cs
@@ -10,5 +10,38 @@ public class Boundary
 
 public class PlayerController : MonoBehaviour
 {
-    
+    public float speed = 2f;
+    public float jumpHeight = 10f;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        Vector2 movement = new Vector2(moveHorizontal, 0);
+        rb.velocity = movement * speed;
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Platform")
+        {
+            StartCoroutine(Jumping());
+        }
+    }
+
+    IEnumerator Jumping()
+    {
+        Physics2D.gravity = new Vector2(0, 40);
+        Physics2D.IgnoreLayerCollision(8, 9, true);
+        yield return new WaitForSeconds(0.3f);
+        Physics2D.gravity = new Vector2(0, 0);
+        Physics2D.IgnoreLayerCollision(8, 9, false);
+        yield return new WaitForSeconds(0.05f);
+        Physics2D.gravity = new Vector2(0, -40);
+    }
 }
