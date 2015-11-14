16ad9a4014f6b02a4b1c54f491e8ae9166644f2e
Mikhaelmurmur <stolovich@yahoo.com>
2015-11-14 21:14:35 +0200

    Initial commit also some assets stuff has been found

---
+++ GameJamKPI/Assets/PlayerController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+[Serializable]
+public class Boundary
+{
+    public float xMin, xMax, yMin, yMax;
+}
+
+public class PlayerController : MonoBehaviour
+{
+    
+}
---
+++ GameJamKPI/Assets/tmpscript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tmpscript : MonoBehaviour {
+
+    // Use this for initialization
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Platform")
+        {
+            collision.rigidbody.AddRelativeForce(transform.up * 100, ForceMode2D.Impulse);
+            //rb.velocity = new Vector2(0, jumpHeight);
+            Debug.Log("Collision entered");
+            //Vector2 movement = new Vector2(0, jumpHeight);
+            //rb.velocity = movement;
+            //StartCoroutine(Jumping());
+            //transform.Translate(transform.up*50*Time.fixedDeltaTime);
+            //Physics2D.gravity = new Vector2(0,10);
+            //rb.AddForce(transform.up *3000);
+        }
+    }
+}
