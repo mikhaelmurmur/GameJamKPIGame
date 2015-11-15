21fac4e9a1962bb38dcfab3f6fce12042533365e
Mikhaelmurmur <stolovich@yahoo.com>
2015-11-15 14:12:49 +0200

    Spawn is done

--- GameJamKPI/Assets/Scripts/BouncingFloor.cs
+++ GameJamKPI/Assets/Scripts/BouncingFloor.cs
@@ -4,10 +4,26 @@ using System.Collections;
 public class BouncingFloor : MonoBehaviour
 {
     public float bounceFactor = 4f;
+    public GameObject spawner;
+
+    private bool hasSpawned = false;
+
+    void Start()
+    {
+        spawner = GameObject.FindWithTag("Respawn");
+
+    }
+
     void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log("Trigger entered");
         other.gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
         other.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * bounceFactor, ForceMode2D.Impulse);
+
+        if (!hasSpawned)
+        {
+            spawner.SendMessage("Spawn");
+            hasSpawned = true;
+        }
     }
 }
--- GameJamKPI/Assets/Scripts/PlayerController.cs
+++ GameJamKPI/Assets/Scripts/PlayerController.cs
@@ -19,29 +19,11 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
     }
 
-    void FixedUpdate()
+    private void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
-        Vector2 movement = new Vector2(moveHorizontal*speed, rb.velocity.y);
+        Vector2 movement = new Vector2(moveHorizontal * speed, rb.velocity.y);
         rb.velocity = movement;
     }
 
-    //void OnCollisionEnter2D(Collision2D other)
-    //{
-    //    if (other.gameObject.tag == "Platform")
-    //    {
-    //        StartCoroutine(Jumping());
-    //    }
-    //}
-
-    //IEnumerator Jumping()
-    //{
-    //    Physics2D.gravity = new Vector2(0, 40);
-    //    Physics2D.IgnoreLayerCollision(8, 9, true);
-    //    yield return new WaitForSeconds(0.3f);
-    //    Physics2D.gravity = new Vector2(0, 0);
-    //    Physics2D.IgnoreLayerCollision(8, 9, false);
-    //    yield return new WaitForSeconds(0.05f);
-    //    Physics2D.gravity = new Vector2(0, -40);
-    //}
 }
