a21c109d4fb433dfb2f3d9d32fb793bd153efee7
Mikhaelmurmur <stolovich@yahoo.com>
2015-11-15 10:59:26 +0200

    Jumping is done

--- GameJamKPI/Assets/Scripts/BouncingFloor.cs
+++ GameJamKPI/Assets/Scripts/BouncingFloor.cs
@@ -3,23 +3,11 @@ using System.Collections;
 
 public class BouncingFloor : MonoBehaviour
 {
-
     public float bounceFactor = 4f;
-    // Use this for initialization
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     void OnTriggerEnter2D(Collider2D other)
     {
+        Debug.Log("Trigger entered");
         other.gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-        other.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 1, ForceMode2D.Impulse);
+        other.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * bounceFactor, ForceMode2D.Impulse);
     }
 }
---
+++ GameJamKPI/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+[Serializable]
+public class Boundary
+{
+    public float xMin, xMax, yMin, yMax;
+}
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 2f;
+    public float jumpHeight = 10f;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        Vector2 movement = new Vector2(moveHorizontal*speed, rb.velocity.y);
+        rb.velocity = movement;
+    }
+
+    //void OnCollisionEnter2D(Collision2D other)
+    //{
+    //    if (other.gameObject.tag == "Platform")
+    //    {
+    //        StartCoroutine(Jumping());
+    //    }
+    //}
+
+    //IEnumerator Jumping()
+    //{
+    //    Physics2D.gravity = new Vector2(0, 40);
+    //    Physics2D.IgnoreLayerCollision(8, 9, true);
+    //    yield return new WaitForSeconds(0.3f);
+    //    Physics2D.gravity = new Vector2(0, 0);
+    //    Physics2D.IgnoreLayerCollision(8, 9, false);
+    //    yield return new WaitForSeconds(0.05f);
+    //    Physics2D.gravity = new Vector2(0, -40);
+    //}
+}
