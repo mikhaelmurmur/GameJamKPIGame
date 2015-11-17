d0b0eb6587325d7801e8bc5986b87f7d22b677eb
Mikhaelmurmur <stolovich@yahoo.com>
2015-11-17 09:14:45 +0200

    Destroyer is added

---
+++ GameJamKPI/Assets/Scripts/DestroyerController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyerController : MonoBehaviour
+{
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("Player is dead!");
+            EventManager.Instance.CallEvent(GlobalEvents.GameIsOver, new object[] { });
+        }
+        Debug.Log("Destroy!");
+        Destroy(other.gameObject);
+    }
+
+}
