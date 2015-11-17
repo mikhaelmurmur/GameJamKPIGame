d522bdfb6e638a8d322d968955d94ef125f01a49
Mikhaelmurmur <stolovich@yahoo.com>
2015-11-18 00:50:13 +0200

    betta Build

---
+++ GameJamKPI/Assets/Scripts/BoundaryController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoundaryController : MonoBehaviour
+{
+    void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log("Hello!");
+        other.transform.position = new Vector3(-other.transform.position.x, other.transform.position.y);
+    }
+}
